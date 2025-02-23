e8831374a567ab1498b656c131d0ce54564b6784
Blackbeard348 <cmorgan348@outlook.com>
2025-02-23 20:45:21 +0800

    GitHub Pages update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://github.com/BlackBeard348/learning_react.git",
+  base: "/learning_react",
   plugins: [react()],
 })
